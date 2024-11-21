66968695ce5ade030c5e65761661df780ea80365
EnderiteFox <theopariney70110@gmail.com>
2024-11-21 15:10:25 +0100

    Added window

--- code/Main.cc
+++ code/Main.cc
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <gf/Window.h>
+#include <gf/RenderWindow.h>
+#include <gf/Event.h>
+#include <gf/Color.h>
 
 int main() {
-  std::cout << "Hello World" << std::endl;
-  return 0;
+    // Creating window and renderer
+
+    gf::Window window("Platformer", {1920, 1080});
+    gf::RenderWindow renderer(window);
+
+    renderer.clear(gf::Color::White);
+
+    // Game loop
+
+    while (window.isOpen()) {
+
+        gf::Event event{};
+
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case gf::EventType::Closed:
+                    window.close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        renderer.clear();
+        renderer.display();
+    }
+
+    return 0;
 }
