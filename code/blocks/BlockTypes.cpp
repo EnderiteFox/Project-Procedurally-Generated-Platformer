78b87949733225a8410fc514dcca638d7961700a
Unity2654 <k.jelic.74@gmail.com>
2025-01-04 18:45:06 +0000

    Added comments to the revamped BlockTypes class

--- code/blocks/BlockTypes.cpp
+++ code/blocks/BlockTypes.cpp
@@ -24,29 +24,37 @@ namespace platformer {
     }
 
     std::pair<std::string,std::map<std::string,std::string>> BlockTypes::parseLine(std::string line){
+        // Finding the beginning of the line
         std::size_t i = line.find("<")+1;
         size_t size = line.size();
         std::string tagName;
         std::map<std::string,std::string> elements;
-        // Tag
+
+        // Reading the tag of the line
         while(line[i] != '>' && line[i] != ' '){
             tagName += line[i];
             i++;
         }
+
+        // If the line only contains the tag, returns it
         if(line[i] == '>'){
             return {tagName,elements};
         }
+
+        //Reading the elements of the tag
         i++;
-        // Rest of the elements
         while(line[i] != '>' && i < size){
             while(line[i]==' ') i++;
             std::string tag;
             std::string value;
+
+            // First, the key (everything before the =)
             while (line[i] != '='){
                 tag += line[i];
                 i++;
             }
             i+=2;
+            // The value (everything between two quotes, after the equal)
             while(line[i] != '"'){
                 value += line[i];
                 i++;
@@ -58,22 +66,24 @@ namespace platformer {
     }
 
     void BlockTypes::parseXML(){
+        // Opening the file
         std::ifstream file("../assets/tiles.xml");
         assert(file);
         std::string texturepaths = "";
-        for(std::string line; std::getline(file, line); )
-        {
-            if(line.find("<!--") == 0) continue;
 
+        // Reading the file line by line
+        for(std::string line; std::getline(file, line); ){
+            if(line.find("<!--") == 0) continue; // Skipping the line if it's a comment
 
             auto[tagName,tags] = parseLine(line);
 
+            // First line, which only elements should be gfxpath
             if(texturepaths == ""){
                 texturepaths = tags["gfxpath"];
                 continue;
             }
 
-            if (tagName[0] == '/') continue;
+            if (tagName[0] == '/') continue; // Skipping the lines that closes tags. There should be only one : at the end
 
             BlockTypes::cache.emplace(tags["type"],BlockType(tags["type"],"../"+texturepaths+"/"+tags["texture"]));
         }
--- code/blocks/BlockTypes.h
+++ code/blocks/BlockTypes.h
@@ -1,5 +1,6 @@
 /**
- * A container for all the existing block types
+ * A static class used to manage types of blocks.
+ * Types of blocks are stored in a .xml file within the assets file
  * Destined to be used by the block manager
  */
 #pragma once
@@ -11,16 +12,26 @@
 namespace platformer {
     class BlockTypes {
     public:
+        // Returns all the existing types of block
         static std::vector<BlockType> getAllTypes();
 
+        // Get the data of a type of block by it's name. It's name can be found within the XML file
         static BlockType getBlockTypeByName(std::string name);
 
     private:
-
+        // Contains the data stored in the XML file once it has been parsed once, to avoid reading the file again and again.
+        // If the file doesn't exists, the game crashes at launch.
         static std::map<std::string,BlockType> cache;
 
+        // Parse a line from the XML file
+        // Returns a pair of :
+        //  - The tag of the line
+        //  - A map matching an element of the tag with it's value
+        // If the file is not well formated, creates a segmentation fault.
         static std::pair<std::string,std::map<std::string,std::string>> parseLine(std::string line);
 
+        // Parse the XML file and store it's data in the cache
+        // It's not a complete XML parser, and only works with the specific format we use for this project.
         static void parseXML();
     };
 }
