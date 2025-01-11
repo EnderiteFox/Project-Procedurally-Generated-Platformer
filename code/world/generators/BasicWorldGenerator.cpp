6897812ff33d18c3e05fbe9987817f0eb802c156
EnderiteFox <theopariney70110@gmail.com>
2025-01-11 14:31:25 +0100

    Made rooms generate in multiple sizes

--- code/world/generators/BasicWorldGenerator.cpp
+++ code/world/generators/BasicWorldGenerator.cpp
@@ -15,49 +15,98 @@ namespace platformer {
     void BasicWorldGenerator::generate(World& world) {
         std::cout << "Generating seed " << seed << "\n";
         generateRooms();
+        if (rooms.empty()) return;
         fillWorld(world);
         carveRooms(world);
         generatePath();
         debugPath(world);
-        for (const gf::Vector2i room : rooms) {
+        for (const gf::Vector4i room : rooms) {
             if (const std::optional<gf::Vector2f> spawnpoint = findValidSpawnpoint(world, room)) {
                 world.setSpawnPoint(spawnpoint.value());
                 break;
             }
-            std::cout << "Failed to find spawnpoint, trying again\n";
         }
     }
 
     void BasicWorldGenerator::generateRooms() {
-        rooms.push_back(gf::Vector2i{0, 0});
+        rooms.push_back(gf::Vector4i{
+            0,
+            0,
+            random.computeUniformInteger(MIN_ROOM_HEIGHT, MAX_ROOM_HEIGHT),
+            random.computeUniformInteger(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH)
+        });
+
 
         for (int i = 1; i < ROOM_COUNT; ++i) {
-            gf::Vector2i top = {rooms.back().x, rooms.back().y - ROOM_HEIGHT};
-            gf::Vector2i right = {rooms.back().x + ROOM_WIDTH, rooms.back().y};
-            gf::Vector2i bottom = {rooms.back().x, rooms.back().y + ROOM_HEIGHT};
-            gf::Vector2i left = {rooms.back().x - ROOM_WIDTH, rooms.back().y};
-
-            std::vector<gf::Vector2i> possibleDirections;
-            if (std::find(rooms.cbegin(), rooms.cend(), top) == rooms.cend()) {
-                possibleDirections.push_back(top);
-            }
-            if (std::find(rooms.cbegin(), rooms.cend(), right) == rooms.cend()) {
-                possibleDirections.push_back(right);
-            }
-            if (std::find(rooms.cbegin(), rooms.cend(), bottom) == rooms.cend()) {
-                possibleDirections.push_back(bottom);
-            }
-            if (std::find(rooms.cbegin(), rooms.cend(), left) == rooms.cend()) {
-                possibleDirections.push_back(left);
+            if (rooms.empty()) {
+                std::cerr << "Unable to generate any room\n";
+                return;
             }
 
-            rooms.push_back(
-                possibleDirections.at(
-                    random.computeUniformInteger(0, static_cast<int>(possibleDirections.size() - 1))
-                )
-            );
+            if (!generateRoomNextToRoom(rooms.back())) {
+                std::cout << "Failed to generate room, going back\n";
+                rooms.pop_back();
+                i--;
+            }
         }
     }
+
+    bool BasicWorldGenerator::generateRoomNextToRoom(const gf::Vector4i previousRoom) {
+        for (int sizeTries = 0; sizeTries < MAX_ROOMGEN_SIZE_TRIES; ++sizeTries) {
+            gf::Vector4i room{
+                0,
+                0,
+                random.computeUniformInteger(MIN_ROOM_HEIGHT, MAX_ROOM_HEIGHT),
+                random.computeUniformInteger(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH)
+            };
+            for (int posTries = 0; posTries < MAX_ROOMGEN_DIRECTION_TRIES; ++posTries) {
+                switch (random.computeUniformInteger(0, 3)) {
+                case 0:
+                    room.x = previousRoom.x - room.w;
+                    room.y = random.computeUniformInteger(
+                        previousRoom.y - room.z + MIN_ROOM_ENTRANCE_SIZE,
+                        previousRoom.y + previousRoom.z - MIN_ROOM_ENTRANCE_SIZE
+                    );
+                    break;
+                case 1:
+                    room.x = random.computeUniformInteger(
+                        previousRoom.x - room.w + MIN_ROOM_ENTRANCE_SIZE,
+                        previousRoom.x + previousRoom.w - MIN_ROOM_ENTRANCE_SIZE
+                    );
+                    room.y = previousRoom.y - room.z;
+                    break;
+                case 2:
+                    room.x = previousRoom.x + previousRoom.w;
+                    room.y = random.computeUniformInteger(
+                        previousRoom.y - room.z + MIN_ROOM_ENTRANCE_SIZE,
+                        previousRoom.y + previousRoom.z - MIN_ROOM_ENTRANCE_SIZE
+                    );
+                    break;
+                default:
+                    room.x = random.computeUniformInteger(
+                        previousRoom.x - room.w + MIN_ROOM_ENTRANCE_SIZE,
+                        previousRoom.x + previousRoom.w - MIN_ROOM_ENTRANCE_SIZE
+                    );
+                    room.y = previousRoom.y + previousRoom.z;
+                }
+
+                if (
+                    std::none_of(
+                        rooms.cbegin(),
+                        rooms.cend(),
+                        [room](const gf::Vector4i otherRoom) {
+                                return areIntersecting(room, otherRoom);
+                        }
+                    )
+                ) {
+                    rooms.push_back(room);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     void BasicWorldGenerator::fillWorld(World& world) {
         int minX = 0;
@@ -65,11 +114,11 @@ namespace platformer {
         int minY = 0;
         int maxY = 0;
 
-        for (const gf::Vector2i room : rooms) {
+        for (const gf::Vector4i room : rooms) {
             if (room.x < minX) minX = room.x;
-            if (room.x + ROOM_WIDTH > maxX) maxX = room.x + ROOM_WIDTH;
+            if (room.x + room.w > maxX) maxX = room.x + room.w;
             if (room.y < minY) minY = room.y;
-            if (room.y + ROOM_HEIGHT > maxY) maxY = room.y + ROOM_HEIGHT;
+            if (room.y + room.z > maxY) maxY = room.y + room.z;
         }
 
         for (int x = minX - 1; x <= maxX; ++x) {
@@ -81,9 +130,9 @@ namespace platformer {
     }
 
     void BasicWorldGenerator::carveRooms(const World& world) {
-        for (const gf::Vector2i room : rooms) {
-            for (int x = room.x; x < room.x + ROOM_WIDTH; ++x) {
-                for (int y = room.y; y < room.y + ROOM_HEIGHT; ++y) {
+        for (const gf::Vector4i room : rooms) {
+            for (int x = room.x; x < room.x + room.w; ++x) {
+                for (int y = room.y; y < room.y + room.z; ++y) {
                     world.getBlockManager().removeBlockAt(x, y);
                 }
             }
@@ -92,10 +141,10 @@ namespace platformer {
 
 
     void BasicWorldGenerator::generatePath() {
-        for (const gf::Vector2i room : rooms) {
+        for (const gf::Vector4i room : rooms) {
             path.push_back(gf::Vector2i{
-                room.x + random.computeUniformInteger(0, ROOM_WIDTH - 1),
-                room.y + random.computeUniformInteger(0, ROOM_HEIGHT - 1)
+                room.x + random.computeUniformInteger(0, room.w - 1),
+                room.y + random.computeUniformInteger(0, room.z - 1)
             });
         }
     }
@@ -111,16 +160,16 @@ namespace platformer {
         return blockManager.isEmptyBlock(x, y) && !blockManager.isEmptyBlock(x, y + 1);
     }
 
-    std::optional<gf::Vector2f> BasicWorldGenerator::findValidSpawnpoint(const World& world, const gf::Vector2i room) const {
-        for (int y = room.y + ROOM_HEIGHT - 1; y >= room.y; --y) {
-            for (int x = room.x + ROOM_WIDTH / 2; x < room.x + ROOM_WIDTH; ++x) {
+    std::optional<gf::Vector2f> BasicWorldGenerator::findValidSpawnpoint(const World& world, const gf::Vector4i room) {
+        for (int y = room.y + room.z - 1; y >= room.y; --y) {
+            for (int x = room.x + room.w / 2; x < room.x + room.w; ++x) {
                 if (isValidSpawnpoint(world, x, y)) {
                     return std::make_optional(
                         gf::Vector2f{static_cast<float>(x), static_cast<float>(y)} * world.getBlockManager().BLOCK_SIZE
                     );
                 }
             }
-            for (int x = room.x + ROOM_WIDTH / 2 - 1; x >= room.x; --x) {
+            for (int x = room.x + room.w / 2 - 1; x >= room.x; --x) {
                 if (isValidSpawnpoint(world, x, y)) {
                     return std::make_optional(
                         gf::Vector2f{static_cast<float>(x), static_cast<float>(y)} * world.getBlockManager().BLOCK_SIZE
@@ -130,5 +179,13 @@ namespace platformer {
         }
         return std::nullopt;
     }
+
+    bool BasicWorldGenerator::areIntersecting(const gf::Vector4i room1, const gf::Vector4i room2) {
+        return room1.x + room1.w > room2.x
+            && room1.x < room2.x + room2.w
+            && room1.y + room1.z > room2.y
+            && room1.y < room2.y + room2.z;
+    }
+
 
 } // platformer
--- code/world/generators/BasicWorldGenerator.h
+++ code/world/generators/BasicWorldGenerator.h
@@ -11,26 +11,51 @@
 namespace platformer {
     class BasicWorldGenerator final : public WorldGenerator {
         // Placeholder values, will be defined later
-        const int ROOM_WIDTH = 3;
-        const int ROOM_HEIGHT = 3;
         const int ROOM_COUNT = 10;
+
+        const int MIN_ROOM_WIDTH = 3;
+        const int MAX_ROOM_WIDTH = 7;
+
+        const int MIN_ROOM_HEIGHT = 3;
+        const int MAX_ROOM_HEIGHT = 7;
+
+        const int MAX_ROOMGEN_SIZE_TRIES = 10;
+        const int MAX_ROOMGEN_DIRECTION_TRIES = 2 * MAX_ROOM_WIDTH + 2 * MAX_ROOM_HEIGHT;
+
+        const int MIN_ROOM_ENTRANCE_SIZE = 2;
 
         const std::string WALL_BLOCK_NAME = "testBlock";
 
         const BlockType WALL_BLOCK = BlockTypes::getBlockTypeByName(WALL_BLOCK_NAME);
 
-        std::vector<gf::Vector2i> rooms;
+        /*
+         * The list of rooms
+         * The coordinate of the room is at the top left of the room
+         * x: The x room coordinate
+         * y: The y room coordinate
+         * z: The height of the room
+         * w: The width of the room
+         */
+        std::vector<gf::Vector4i> rooms;
         std::vector<gf::Vector2i> path;
 
 
         void generateRooms();
+        /**
+         * Generates a room next to another room
+         * @param previousRoom The room to generate a room next to
+         * @return true if the room was successfully generated, false otherwise
+         */
+        bool generateRoomNextToRoom(gf::Vector4i previousRoom);
         void fillWorld(World& world);
         void carveRooms(const World& world);
 
         void generatePath();
         void debugPath(const World& world);
 
-        std::optional<gf::Vector2f> findValidSpawnpoint(const World& world, gf::Vector2i room) const;
+        static std::optional<gf::Vector2f> findValidSpawnpoint(const World& world, gf::Vector4i room);
+
+        static bool areIntersecting(gf::Vector4i room1, gf::Vector4i room2);
 
     public:
         BasicWorldGenerator();
