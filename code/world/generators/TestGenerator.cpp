58f7c815a0e89f4f81a5944017572b395c3297ce
Unity2654 <k.jelic.74@gmail.com>
2025-01-12 19:06:05 +0000

    Added ladders, without their behaviour.

--- code/world/generators/TestGenerator.cpp
+++ code/world/generators/TestGenerator.cpp
@@ -2,6 +2,7 @@
 
 #include <blocks/BlockTypes.h>
 #include <world/World.h>
+#include <iostream>
 
 namespace platformer {
     void TestGenerator::generate(World& world) {
@@ -17,6 +18,10 @@ namespace platformer {
         }
         for (int i = 6; i <= 10; ++i) {
             blockManager.setBlockTypeAt(i, 0, BlockTypes::getBlockTypeByName("jellyBlock"));
+        }
+        for (int i=-2; i>=-4; --i){
+            assert(BlockTypes::getBlockTypeByName("testLadder").type != "");
+            blockManager.setBlockTypeAt(-6, i, BlockTypes::getBlockTypeByName("testLadder"));
         }
 
         world.setSpawnPoint(gf::Vector2f{5.0f, -20.0f});
